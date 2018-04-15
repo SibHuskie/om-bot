b7a41ddc75c98af8ce17e4093a6ce0d26139893f
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-04-15 17:31:26 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,4 +16,23 @@ async def on_ready():
     print(client.user.id)
     print("---------------")
     await client.change_presence(game=discord.Game(name='with Huskie'))
+    
+# <echo <text>
+@client.command(pass_context=True)
+async def echo(ctx, *, args=None): 
+    staff_role = discord.utils.get(ctx.message.server.roles, name='Staff')
+    author = ctx.message.author
+    msg = discord.Embed(colour=0x7f8a9b, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if staff_role in author.roles or staff_role in author.roles:
+        if args == None:
+            msg.add_field(name=":warning: ", value="<say <text>")
+            await client.say(embed=msg)
+        else:
+            await client.say("{}".format(args))
+            await client.delete_message(ctx.message)
+    else:
+        msg.add_field(name=":warning: ", value="`This command can only be used by Staff!`")
+        await client.say(embed=msg)
 client.run(os.environ['BOT_TOKEN'])
